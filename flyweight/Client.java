74f697afa9fa7667462afa8b760f8a344cba6997
YuanGaoPeng <yuangaopeng@csii.com.cn>
2019-09-04 10:07:20 +0800

    享元模式

--- flyweight/Client.java
+++ flyweight/Client.java
@@ -1,16 +1,23 @@
 package com.demo.flyweight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author yuan
  */
 public class Client {
     public static void main(String[] args) {
         BookOrderFactory factory = new BookOrderFactory();
-        factory.getBookOrder("三国").print();
-        factory.getBookOrder("水浒").print();
-        factory.getBookOrder("三国").print();
-        factory.getBookOrder("水浒").print();
-        factory.getBookOrder("水浒").print();
-        factory.getBookOrder("三国").print();
+        List<BookOrder> list = new ArrayList<>();
+        list.add(factory.getBookOrder("三国"));
+        list.add(factory.getBookOrder("水浒"));
+        list.add(factory.getBookOrder("三国"));
+        list.add(factory.getBookOrder("水浒"));
+        list.add(factory.getBookOrder("水浒"));
+        list.add(factory.getBookOrder("三国"));
+        for(BookOrder order : list){
+            order.print();
+        }
     }
 }
